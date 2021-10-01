aed81af5284e4261a5ec34ec05a8615270f77574
Arsom Mahatthanamongkol <arsom.ma@cdg.co.th>
2021-10-01 12:05:41 +0700

    init project

---
+++ src/main/java/th/co/cdgs/day2/SpringBootDay2Application.java
@@ -0,0 +1,13 @@
+package th.co.cdgs.day2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDay2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDay2Application.class, args);
+	}
+
+}
---
+++ src/test/java/th/co/cdgs/day2/SpringBootDay2ApplicationTests.java
@@ -0,0 +1,13 @@
+package th.co.cdgs.day2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDay2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
