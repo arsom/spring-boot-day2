5f13eda1b7b77f0d753635c1c3e206f1521dda55
Arsom Mahatthanamongkol <arsom.ma@cdg.co.th>
2021-10-01 16:41:46 +0700

    employee repository

---
+++ src/test/java/th/co/cdgs/day2/employee/EmployeeRepositoryTest.java
@@ -0,0 +1,29 @@
+package th.co.cdgs.day2.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+public class EmployeeRepositoryTest {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Test
+    public void case01(){
+        Employee demo  = new Employee();
+        demo.setName("Arsom");
+        employeeRepository.save(demo);
+
+        Optional<Employee> result = employeeRepository.findById(1);
+        assertTrue(result.isPresent());
+        assertEquals(1,result.get().getId());
+        assertEquals("Arsom",result.get().getName());
+    }
+
+}
