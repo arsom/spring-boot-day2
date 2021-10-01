4ae56b9befeab7512d67cc59bc684b2318d77e48
Arsom Mahatthanamongkol <arsom.ma@cdg.co.th>
2021-10-01 16:59:55 +0700

    add testing

--- src/main/java/th/co/cdgs/day2/employee/EmployeeService.java
+++ src/main/java/th/co/cdgs/day2/employee/EmployeeService.java
@@ -1,11 +1,21 @@
 package th.co.cdgs.day2.employee;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class EmployeeService {
 
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
     public EmployeeReponse getEmployeeById(int id) {
+       Optional<Employee> result =  employeeRepository.findById(id);
+       if(!result.isPresent()){
+           throw  new RuntimeException("employee not found with id = "+ id);
+       }
         return new EmployeeReponse(id,"Arsom");
     }
 }
--- src/test/java/th/co/cdgs/day2/employee/EmployeeControllerTest.java
+++ src/test/java/th/co/cdgs/day2/employee/EmployeeControllerTest.java
@@ -1,5 +1,6 @@
 package th.co.cdgs.day2.employee;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,11 +16,24 @@ public class EmployeeControllerTest {
     @Autowired
     TestRestTemplate restTemplate;
 
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @AfterEach
+    public void clearData(){
+        employeeRepository.deleteAll();
+    }
+
     @Test
     public void getById(){
+        Employee demo  = new Employee();
+        demo.setName("Arsom");
+        employeeRepository.save(demo);
         EmployeeReponse result = restTemplate.getForObject("/employee/1",EmployeeReponse.class);
         assertEquals(1,result.getId());
         assertEquals("Arsom",result.getName());
+
+
     }
 
     @Test
