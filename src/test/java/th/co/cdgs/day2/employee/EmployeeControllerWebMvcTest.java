3cca32e1373506127576bc254dc2855aa0cd8ef7
Arsom Mahatthanamongkol <arsom.ma@cdg.co.th>
2021-10-01 15:41:52 +0700

    mock employee by id web mvc

--- src/test/java/th/co/cdgs/day2/employee/EmployeeControllerWebMvcTest.java
+++ src/test/java/th/co/cdgs/day2/employee/EmployeeControllerWebMvcTest.java
@@ -2,8 +2,10 @@ package th.co.cdgs.day2.employee;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -19,13 +21,17 @@ public class EmployeeControllerWebMvcTest {
     @Autowired
     private MockMvc mvc;
 
+    @MockBean
+    private EmployeeService employeeService;
+
     @Test
     public void getById() throws  Exception{
+        Mockito.when(employeeService.getEmployeeById(1)).thenReturn(new EmployeeReponse(1,"Lek"));
         MvcResult mvcResult = mvc.perform(get("/employee/1")).andExpect(status().isOk()).andReturn();
         ObjectMapper mapper = new ObjectMapper();
         EmployeeReponse employeeReponse = mapper.readValue(mvcResult.getResponse().getContentAsByteArray(),EmployeeReponse.class);
         assertEquals(1,employeeReponse.getId());
-        assertEquals("Arsom",employeeReponse.getName());
+        assertEquals("Lek",employeeReponse.getName());
 
     }
 }
